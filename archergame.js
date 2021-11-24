994a27eafe34f879dc03c056c6f729d2314e36d4
Archisha Datta <archisha.datta@gmail.com>
2021-11-23 21:08:17 -0800

    add textures

--- archergame.js
+++ archergame.js
@@ -1,31 +1,17 @@
 import { defs, tiny } from './examples/common.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Scene, Shader, Material, Texture,
 } = tiny;
+
+const {Rounded_Capped_Cylinder, Square, Axis_Arrows, Textured_Phong, Grid_Sphere} = defs
 
 //variables
 let angle = 45;
 let speed = 40;
 let t;
 
-class Cube extends Shape {
-    constructor() {
-        super("position", "normal",);
-        // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
-        this.arrays.position = Vector3.cast(
-            [-1, -1, -1], [1, -1, -1], [-1, -1, 1], [1, -1, 1], [1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1],
-            [-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [-1, 1, 1], [1, -1, 1], [1, -1, -1], [1, 1, 1], [1, 1, -1],
-            [-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1], [1, -1, -1], [-1, -1, -1], [1, 1, -1], [-1, 1, -1]);
-        this.arrays.normal = Vector3.cast(
-            [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
-            [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0],
-            [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
-        // Arrange the vertices into a square shape in texture space too:
-        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
-            14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
-    }
-}
+
 
 class Base_Scene extends Scene {
     /**
@@ -38,8 +24,14 @@ class Base_Scene extends Scene {
         this.hover = this.swarm = false;
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
-            'cube': new Cube(),
+            square: new Square(),
+            ground: new Square(),
+            tower: new Square(),
+            bomb: new Grid_Sphere(),
         };
+
+        this.shapes.ground.arrays.texture_coord = this.shapes.ground.arrays.texture_coord.map(v => vec(v[0]*3, v[1]*3))
+
 
         // *** Materials
         this.materials = {
@@ -47,8 +39,33 @@ class Base_Scene extends Scene {
                 {
                     ambient: .4,
                     diffusivity: .6,
-                    color: hex_color("#ffffff")
+                    color: hex_color("#ff0000")
                 }),
+             tank: new Material(new Textured_Phong(), {
+                color: hex_color("#000000"),
+                ambient: 0.9, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/tank.png", "LINEAR_MIPMAP_LINEAR")
+             }),
+             tower: new Material(new Textured_Phong(), {
+                color: hex_color("#000000"),
+                ambient: 0.9, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/metal.png", "LINEAR_MIPMAP_LINEAR")
+             }),
+             grass: new Material(new Textured_Phong(), {
+                color: hex_color("#000000"),
+                ambient: 0.9, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/grass.jpg", "LINEAR_MIPMAP_LINEAR")
+             }),
+             bunker: new Material(new Textured_Phong(), {
+                color: hex_color("#000000"),
+                ambient: 0.9, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/bunker.png", "LINEAR_MIPMAP_LINEAR")
+             }),
+             bomb: new Material(new Textured_Phong(), {
+                color: hex_color("#000000"),
+                ambient: 0.9, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/bomb.png", "LINEAR_MIPMAP_LINEAR")
+             })
         };
     }
 
@@ -93,14 +110,6 @@ class Element {
 
     // Call once per frame
     update(context, program_state) {
-        //const dx = (speed/100) * Math.cos(angle);
-        //const dy = (speed/100) * Math.sin(angle) - 9.81*t;
-        //this.dy -= .41
-//         console.log("dy: "+ this.dy)
-//         console.log("dx: "+ this.dx)
-//            t = program_state.animation_time / 1000;
-//         this.x += this.dx;
-//         this.y += this.dy;
         return this.draw(context, program_state);
     }
 }
@@ -117,19 +126,17 @@ class Projectile {
     }
 
     draw(context, program_state) {
+        const t = this.t = program_state.animation_time / 1000;
         let model_transform = Mat4.identity();
         model_transform = model_transform
             .times(Mat4.translation(this.x, this.y, 0))
-            .times(Mat4.scale(this.scale, this.scale, this.zscale));
+            .times(Mat4.scale(this.scale, this.scale, this.zscale))
+            
         return [model_transform, this.material]
     }
 
     // Call once per frame
     update(context, program_state) {
-        //const dx = (speed/100) * Math.cos(angle);
-        //const dy = (speed/100) * Math.sin(angle) - 9.81*t;
-        
-
         if(this.y>0) {
           this.dy -= .00861
           console.log("dy: "+ this.dy)
@@ -159,15 +166,10 @@ export class ArcherGame extends Base_Scene {
         this.origin = new Element(0, 0, 1, this.materials.plastic.override({
             color: hex_color("#ffffff")
         }), 0, 0);
-        this.archer = new Element(-20, 2.5, 5, this.materials.plastic.override({
-            color: hex_color("#ff0000")
-        }), 0, 0);
-        this.target = new Element(20, .5, 3, this.materials.plastic.override({
-            color: hex_color("#00ff00")
-        }), 0, 0);
-        this.ground = new Element(0, -102.5, 100, this.materials.plastic.override({
-            color: hex_color("#964b00")
-        }), 0, 0, 1.1);
+        this.tower = new Element(0, 0, 10, this.materials.tower, 0, 0);
+        this.archer = new Element(-20, 2.5, 5, this.materials.tank, 0, 0);
+        this.target = new Element(20, 2, 5, this.materials.bunker, 0, 0);
+        this.ground = new Element(0, -102.5, 100, this.materials.grass, 0, 0, 1.1);
     }
 
     make_control_panel() {
@@ -185,20 +187,23 @@ export class ArcherGame extends Base_Scene {
     shootProjectile() {
         if (!this.projectile) {
             this.projectile = new Projectile(this.archer.x, this.archer.y, 2,
-                this.materials.plastic.override({
-                    color: hex_color("#0000ff")
-                }),  0.2+(speed/100) * Math.cos(angle*Math.PI/180), 0.2+(speed/100) * Math.sin(angle*Math.PI/180));
-
+                this.materials.bomb,  0.2+(speed/100) * Math.cos(angle*Math.PI/180), 0.2+(speed/100) * Math.sin(angle*Math.PI/180));
         }
     }
 
     drawObjects(context, program_state) {
-        //this.shapes.cube.draw(context, program_state, ...this.origin.update(context, program_state));
-        this.shapes.cube.draw(context, program_state, ...this.archer.update(context, program_state));
-        this.shapes.cube.draw(context, program_state, ...this.ground.update(context, program_state));
-        this.shapes.cube.draw(context, program_state, ...this.target.update(context, program_state));
+
+
+        //this.shapes.square.draw(context, program_state, ...this.origin.update(context, program_state));
+        this.shapes.tower.draw(context, program_state, 
+            Mat4.identity().times(Mat4.scale(3, 10, 1)).times(Mat4.translation(0, 0.75, 0))
+        , this.materials.tower)
+        //this.shapes.tower.draw(context, program_state, ...this.tower.update(context, program_state));
+        this.shapes.square.draw(context, program_state, ...this.archer.update(context, program_state));
+        this.shapes.ground.draw(context, program_state, ...this.ground.update(context, program_state));
+        this.shapes.square.draw(context, program_state, ...this.target.update(context, program_state));
         if (this.projectile) {
-            this.shapes.cube.draw(context, program_state, ...this.projectile.update(context, program_state));
+            this.shapes.square.draw(context, program_state, ...this.projectile.update(context, program_state));
         }
     }
 

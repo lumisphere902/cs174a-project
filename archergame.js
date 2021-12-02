dd80aad2dba5badfdca2e212022f5fc97494e990
Archisha Datta <archisha.datta@gmail.com>
2021-12-01 16:27:23 -0800

    make ground level change every render

--- archergame.js
+++ archergame.js
@@ -8,7 +8,7 @@ const {Rounded_Capped_Cylinder, Square, Axis_Arrows, Textured_Phong, Grid_Sphere
 
 //variables
 let t;
-
+let level = Math.floor(Math.random()*(-95 +125))-125;
 
 
 class Arrow extends Shape {
@@ -39,13 +39,16 @@ class Base_Scene extends Scene {
 
             square: new Square(),
             ground: new Square(),
+            ground2: new Square(),
             tower: new Square(),
             bomb: new Grid_Sphere(),
             arrow: new Arrow(),
+            sky: new Square(),
 
         };
 
         this.shapes.ground.arrays.texture_coord = this.shapes.ground.arrays.texture_coord.map(v => vec(v[0]*3, v[1]*3))
+        this.shapes.ground2.arrays.texture_coord = this.shapes.ground2.arrays.texture_coord.map(v => vec(v[0]*3, v[1]*3))
 
 
         // *** Materials
@@ -80,7 +83,12 @@ class Base_Scene extends Scene {
                 color: hex_color("#000000"),
                 ambient: 0.9, diffusivity: 0.1, specularity: 0.1,
                 texture: new Texture("assets/bomb.png", "LINEAR_MIPMAP_LINEAR")
-             })
+             }),
+             sky: new Material(new Textured_Phong(), {
+                color: hex_color("#000000"),
+                ambient: 0.9, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/sky.png", "LINEAR_MIPMAP_LINEAR")
+             }),
         };
     }
 
@@ -152,7 +160,9 @@ class Projectile {
 
     // Call once per frame
     update(context, program_state) {
-        if(this.y>0) {
+//         console.log(level + 102.5)
+        if(this.y > level + 102.5) {
+           
           this.dy -= .00861
           console.log("dy: "+ this.dy)
           this.x += this.dx;
@@ -199,11 +209,14 @@ export class ArcherGame extends Base_Scene {
         this.origin = new Element(0, 0, 1, this.materials.plastic.override({
             color: hex_color("#ffffff")
         }), 0, 0);
+        
 
         this.tower = new Element(0, 0, 10, this.materials.tower, 0, 0);
+        this.sky = new Element(0, 0, 30, this.materials.sky, 0, -1);
         this.archer = new Element(-20, 2.5, 5, this.materials.tank, 0, 0);
-        this.target = new Element(20, 2, 5, this.materials.bunker, 0, 0);
-        this.ground = new Element(0, -102.5, 100, this.materials.grass, 0, 0, 1.1);        
+        this.target = new Element(20, level + 104.5, 5, this.materials.bunker, 0, 0);
+        this.ground = new Element(-97, -102.5, 100, this.materials.grass, 0, 0, 1.1); 
+        this.ground2 = new Element(97, level, 100, this.materials.grass, 0, 0, 1.1);       
         this.angle = 45;
         this.speed = 50;
     }
@@ -227,10 +240,12 @@ export class ArcherGame extends Base_Scene {
 
     randomize_parameters() {
         this.target.scale = 1 + 10 * Math.random();
-        const min = this.target.scale;
+        const min = 10;
         const max = 30;
         this.target.x = min + Math.random() * (max - min);
-        this.target.y = min + Math.random() * (max - min);
+        this.target.y = level + 104.5 - 4/this.target.scale;
+//         level = Math.floor(Math.random()*(-95 +125))-125;
+//         this.target.y = min + Math.random() * (max - min);
     }
 
     shootProjectile() {
@@ -241,12 +256,13 @@ export class ArcherGame extends Base_Scene {
     }
 
     drawObjects(context, program_state) {
-
+//         this.shapes.sky.draw(context, program_state, ...this.sky.update(context, program_state));
         this.shapes.tower.draw(context, program_state, 
             Mat4.identity().times(Mat4.scale(3, 10, 1)).times(Mat4.translation(0, 0.75, 0))
         , this.materials.tower)
-        //this.shapes.square.draw(context, program_state, ...this.archer.update(context, program_state));
+//         this.shapes.square.draw(context, program_state, ...this.archer.update(context, program_state));
         this.shapes.ground.draw(context, program_state, ...this.ground.update(context, program_state));
+        this.shapes.ground2.draw(context, program_state, ...this.ground2.update(context, program_state));
         this.shapes.square.draw(context, program_state, ...this.target.update(context, program_state));
 
         const [archer_transform, archer_mat] = this.archer.update(context, program_state);
